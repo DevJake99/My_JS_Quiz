ab0bc6637f5383b485dfaa9afc7a98a0ea259b40
DevJake99 <jacobg4@uci.edu>
2023-07-27 22:16:07 -0700

    replaces 1st button after quiz starts to prevent timer from initiating various times

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,10 @@ var startJsQuizBtn = document.querySelector(".topicSelectBtnsJs");
 var timer = document.querySelector(".timer");
 var quizTime = 75;
 
-var optionTwo = document.createElement("button", "class topicSelectBtnsJs");
+var optionOne = document.createElement('button');
+var optionOneContent = document.createTextNode("");
+optionOne.classList.add("topicSelectBtnsJs");
+var optionTwo = document.createElement("button");
 var optionTwoContent = document.createTextNode("");
 optionTwo.classList.add("topicSelectBtnsJs");
 var optionThree = document.createElement("button");
@@ -27,17 +30,22 @@ function startTime() {
 
 }
 
+//fix issue where interval is sped up after multiple button clicks (on the first button only)
 startJsQuizBtn.addEventListener("click", function() {
-    startTime();
     // start quiz when js button is clicked and reveal timer
+    startTime();
+    // removes un-needed elements from main menu
     document.getElementById("subMessage").remove();
     document.getElementById("info").remove();
+    document.getElementById("startJsBtn").remove();
     quizTime--;
     timer.textContent = quizTime + " seconds left.";
     // question 1 will begin 
     document.getElementById("question").innerHTML = "Commonly used data types DO NOT include: ";
     //Change text for button 1
-    startJsQuizBtn.innerHTML = "1. Strings";
+    questionZone.appendChild(optionOne);
+    optionOne.innerHTML = "1. strings"
+    
     // append button 2 and fill text
     questionZone.appendChild(optionTwo);
     optionTwo.innerHTML = "2. booleans";
