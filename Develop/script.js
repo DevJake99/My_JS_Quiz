fdd58644ade291f3bd86daa82bf1e167d5eaab04
DevJake99 <jacobg4@uci.edu>
2023-07-27 23:50:05 -0700

    adds the remaining questions for the quiz plus button functionality of correct answers

--- Develop/script.js
+++ Develop/script.js
@@ -80,7 +80,7 @@ startJsQuizBtn.addEventListener("click", function() {
     
 });
 
-// To move onto the next question, the correct answer (optionTwo in this case) will reveal the next question and options
+// To move onto question 2, the correct answer (optionTwo in this case) will reveal the next question and options
 optionTwo.addEventListener("click", function(){
     document.getElementById("question").innerHTML = 'The condition in an "if/else" statement is enclosed with______.';
     optionOne.innerHTML = "1. quotes";
@@ -90,11 +90,35 @@ optionTwo.addEventListener("click", function(){
 
 });
 
-function gameOver(){
-    if (quizTime == 0){
-        document.getElementsByClassName(".topicSelectBtnsJs").remove();
-        alert("Game Over");
+optionTwo.addEventListener("click", function(){
+    document.getElementById("question").innerHTML = 'Arrays in JavaScript can be used to store______.';
+    optionOne.innerHTML = "numbers and strings";
+    optionTwo.innerHTML = "other arrays";
+    optionThree.innerHTML = 'booleans';
+    optionFour.innerHTML = "all of the above";
+});
 
-    };
-};
-gameOver();
+optionFour.addEventListener("click", function(){
+    document.getElementById("question").innerHTML = " String Values must be enclosed within ____ when being assigned to variables.";
+    optionOne.innerHTML = 'commas';
+    optionTwo.innerHTML = "curly brackets";
+    optionThree.innerHTML = "quotes";
+    optionFour.innerHTML = "parenthesis";
+});
+
+optionThree.addEventListener("click", function(){
+    document.getElementById("question").innerHTML = "A very useful tool during development and debugging for printing conent to the debugger is:";
+    optionOne.innerHTML = "1. JavaScript";
+    optionTwo.innerHTML = "2. terminal/bash";
+    optionThree.innerHTML = "3. for loops";
+    optionFour.innerHTML = "4. console.log";
+});
+
+// function gameOver(){
+//     if (quizTime == 0){
+//         document.getElementsByClassName(".topicSelectBtnsJs").remove();
+//         alert("Game Over");
+
+//     };
+// };
+// gameOver();
